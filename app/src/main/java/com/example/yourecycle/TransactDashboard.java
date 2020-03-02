4a49cd850d3f5687a6f3269570e0e2f9f1f48ba0
raheema12 <ajibolazraheem6@gmail.com>
2020-03-02 11:34:02 +0000

    transaction ui almost done

---
+++ app/src/main/java/com/example/yourecycle/TransactDashboard.java
@@ -0,0 +1,14 @@
+package com.example.yourecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransactDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transact_dashboard);
+    }
+}
